562ad8dd67dc971c5ba468ab3e38fb7c1379e35f
Yuji Koketsu <kyuji7123.public@gmail.com>
2015-06-06 16:35:18 +0900

    change message to location

--- chali_server/app.js
+++ chali_server/app.js
@@ -1,60 +1,52 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var debug = require('debug')('policedb');
+var mongoose = require('mongoose');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 3000);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// mongoose
+var Schema = mongoose.Schema;
+var LocationSchema = new Schema({
+  id: Number,
+  user_id: Number,
+  locate: String,
+  date: Date
 });
+mongoose.model('locations', LocationSchema);
+mongoose.connect('mongodb://localhost/policedb');
+var Locations = mongoose.model('locations');
 
-// error handlers
+// socket.io
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+io.on('connection', function(socket) {
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+  socket.on('location init', function(data) {
+    console.log('location init');
+
+    Locations.find({}).limit(100).exec(function(err, data){
+      socket.emit('location init', data);
     });
   });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+  socket.on('location send', function(data) {
+    console.log('location send');
+
+    data.date = Date.parse(data.date);
+    var location = new location(data);
+    location.save(function(err, location) {
+      if(err) return console.error(err);
+      io.emit('location send', location);
+    });
   });
 });
 
 
-module.exports = app;
+http.listen(app.get('port'), function() {
+  console.log('lisntening on *:' + http.address().port);
+});
